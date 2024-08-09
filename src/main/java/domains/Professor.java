244a9b050e37664b5d30ece67e6acbb787f7f041
Jhonatan <jhonatan.sampaiof@gmail.com>
2024-08-08 23:04:22 -0300

    240808 Add Lombock

--- src/main/java/domains/Professor.java
+++ src/main/java/domains/Professor.java
@@ -1,14 +1,10 @@
 package domains;
 
+import lombok.Data;
+
+@Data
 public class Professor extends Pessoa {
 
     private String disciplina;
 
-    public String getDisciplina() {
-        return disciplina;
-    }
-
-    public void setDisciplina(String disciplina) {
-        this.disciplina = disciplina;
-    }
 }
